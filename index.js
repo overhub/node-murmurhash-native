7161490d89648d509d1be4f264cb57a495605281
Evil Buck <buckleyrobinson@gmail.com>
2024-04-04 17:05:53 -0400

    switching from node-pre-gyp to node-gyp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var binary = require('@mapbox/node-pre-gyp');
+var binary = require('node-gyp-build');
+// var binary = require('@mapbox/node-pre-gyp');
 var path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 module.exports = require(binding_path);
